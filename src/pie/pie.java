bc4ecad3ce4a0154e2bcc3d87fc89a48618fcbee
cezar89 <cezar_algiu@yahoo.com>
2016-01-28 13:13:59 -0500

    INITIAL COMMIT

---
+++ src/pie/pie.java
@@ -0,0 +1,57 @@
+package pie;
+import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner; 
+import java.lang.*;
+
+
+public class pie {
+
+    public static void main(String[] argv) throws IOException {
+
+	Scanner input = new Scanner(System.in);
+	String nameTotal;
+	int i = 0;
+	int j =0;
+	String location;
+	String result1;
+	String name;
+	
+
+	System.out.println("Enter the parts folder location");
+	location=input.nextLine();
+
+	System.out.println("Enter the starting key number ( ex:100 ) , and hit enter");
+	j = input.nextInt();
+
+	 input.close();
+	
+	
+    File folder = new File(location);
+    File[] listOfFiles = folder.listFiles();
+
+    
+
+	while (i < listOfFiles.length) {
+		
+		if (listOfFiles[i].isFile()) {
+			nameTotal=listOfFiles[i].getName();
+
+			name=nameTotal.substring(0, nameTotal.lastIndexOf('.'));
+
+			File f = new File(location + "\\" +nameTotal);
+
+			j++;
+			result1=String.format("%03d",j);
+
+			f.renameTo(new File(location + "\\" +result1+" "+nameTotal));
+			i++;
+		}
+
+	}
+        
+        System.out.println("conversion is done");
+    }
+   
+}
